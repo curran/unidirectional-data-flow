3dab0e79803389a701bda7e24d3fe62b38f9c5b6
Curran Kelleher <68416+curran@users.noreply.github.com>
2024-05-18 09:08:54 -0400

    Update memoize.js

--- memoize.js
+++ memoize.js
@@ -1,40 +1,36 @@
-// `memoize`
 // Akin to React's useMemo, for use in D3-based rendering.
 // Memoizes only one computed value.
 // Accepts a function fn and dependencies, like useMemo.
 // Also accepts a DOM node and a name for the memoization.
 // The memoized value and previous dependencies are stored
 // on the DOM node, using the given name.
-export const memoize = (
-  fn,
-  dependencies,
-  domNodeOrD3Selection,
-  name = 'default',
-) => {
-  const property = `__memoized-${name}`;
-  const domNode = domNodeOrD3Selection.node
-    ? domNodeOrD3Selection.node()
-    : domNodeOrD3Selection;
-  const memoized = domNode[property];
-  if (
-    memoized &&
-    dependencies.length === memoized.dependencies.length
-  ) {
-    let dependenciesChanged = false;
-    for (let i = 0; i < dependencies.length; i++) {
-      if (dependencies[i] !== memoized.dependencies[i]) {
-        dependenciesChanged = true;
-        break;
+export const memoize =
+  (domNodeOrD3Selection, name = 'default') =>
+  (fn, dependencies) => {
+    const property = `__memoized-${name}`;
+    const domNode = domNodeOrD3Selection.node
+      ? domNodeOrD3Selection.node()
+      : domNodeOrD3Selection;
+    const memoized = domNode[property];
+    if (
+      memoized &&
+      dependencies.length === memoized.dependencies.length
+    ) {
+      let dependenciesChanged = false;
+      for (let i = 0; i < dependencies.length; i++) {
+        if (dependencies[i] !== memoized.dependencies[i]) {
+          dependenciesChanged = true;
+          break;
+        }
+      }
+      if (!dependenciesChanged) {
+        return memoized.value;
       }
     }
-    if (!dependenciesChanged) {
-      return memoized.value;
-    }
-  }
 
-  // If we got here, either it's the first run,
-  // or dependencies have changed.
-  const value = fn();
-  domNode[property] = { dependencies, value };
-  return value;
-};
+    // If we got here, either it's the first run,
+    // or dependencies have changed.
+    const value = fn();
+    domNode[property] = { dependencies, value };
+    return value;
+  };
